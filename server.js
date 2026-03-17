63f664b637aeae95ebc9cbe003abd8e912cd8bb6
yarinlevi1301-maker <yarinlevi1301@gmail.com>
2026-03-17 05:19:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -181,4 +181,7 @@ app.post("/bestAnswer",(req,res)=>{
 })
 
 // הפעלת השרת
-app.listen(3000,()=>{ console.log("Server running on http://localhost:3000") })
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT)
+})
